135679dd6f1d1ae4f23b656ea831823336318441
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2024-07-07 18:44:30 +0200

    Added backend routes

--- api/db.js
+++ api/db.js
@@ -1,8 +1,16 @@
-import mysql from "mysql"
+import mysql from "mysql2"
 
 export const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"bakir",
+    password:"bakir123!",
     database:"blog"
-})
+});
+
+db.connect((err) => {
+    if (err) {
+      console.error("Error connecting to the database:", err);
+    } else {
+      console.log("Connected to the database");
+    }
+  });
--- api/index.js
+++ api/index.js
@@ -1,9 +1,20 @@
 import express from "express"
+import cors from "cors";
+import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
+import postRoutes from "./routes/posts.js"
 
 const app = express()
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.listen(8800, ()=>{
-    console.log("Connected")
-})
+app.use(express.json())
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/posts", postRoutes)
+
+
+app.listen(8800, () => {
+    console.log("Connected on port 8800");
+});
